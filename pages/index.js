6eace04016455912d85b2115920583cb353b196f
Manuel Ruh <manuelrugue@gmail.com>
2020-11-27 00:28:57 -0600

    Adding pokemon

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Date from '../components/date'
+import useSWR from 'swr'
 
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function Home({ allPostsData }) {
+
+  const [pokemonNumber] = useState(Math.floor(Math.random() * 100 + 1));
+
+  const { data: pokemonData, error } = useSWR(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`, fetcher);
+
+  let pokemonView = null;
+  if(pokemonData) {
+    
+    pokemonView = (
+      <>
+        <p>My pokemon for you today: <b>{pokemonData.name}</b></p>
+        <Link href={`pokemon\\${pokemonNumber}`}><img src={pokemonData.sprites.front_default} /></Link>
+      </>
+    )
+  }
+
   return (
     <Layout home>
       <Head>
@@ -14,6 +33,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Yo, Manuel in da haus</p>
+        {pokemonView}
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
